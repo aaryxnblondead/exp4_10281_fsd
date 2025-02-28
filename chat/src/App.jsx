fbdf6704de075beed49ddab5da01f09950b0118a
Aaryan Singh <aaryansingh2810@gmail.com>
2025-02-28 11:22:54 +0530

    a

---
+++ chat/src/App.jsx
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBGCsRF2xeq4LwQvUHbCigUzekpwC3V-0E",
+  authDomain: "react-chat-app-c4e9f.firebaseapp.com",
+  projectId: "react-chat-app-c4e9f",
+  storageBucket: "react-chat-app-c4e9f.firebasestorage.app",
+  messagingSenderId: "309926381215",
+  appId: "1:309926381215:web:27d699046d2972ae3f84aa",
+  measurementId: "G-RG47KH1GMD"})
+
+function App(){
+}
+
+export default App;
